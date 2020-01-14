df8d94a5f5b0c67f43800c8c9e32982ef921f156
jaimatadi <sumitneema1988@gmail.com>
2020-01-14 20:38:01 +0530

    added neo4j support

--- src/server/routes/other-routes.js
+++ src/server/routes/other-routes.js
@@ -1,4 +1,20 @@
 const app = require('express');
 const router = app.Router();
 
+const Neode = require('neode');
+
+const instance = new Neode('bolt+routing://7374876f.databases.neo4j.io',
+    'neo4j', 'l_-P6kF1rP5ozx96-t-nDLL0AsDseHF_n6ssJefQRoY');
+
+// for more information about neode visit
+// https://medium.com/neo4j/interacting-with-neo4j-in-nodejs-using-the-neode-object-mapper-3d99cb324546
+
+// run a cypher
+instance.cypher(
+    'MATCH (p:ns4__prefixIRI {rdfs__label: {name}}) RETURN p', { name: "notochord" })
+    .then(res => {
+        console.log("check record")
+        console.log(res.records.length);
+    })
+
 module.exports = router;
