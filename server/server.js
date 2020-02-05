5b180cb22dcb074b37a6ec9841d074a7fa313bc7
jaimatadi <mandhan.priyanka@gmail.com>
2020-02-05 20:56:53 +0530

    mba urls

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // to be able to read environment variables 
 // const dotenv = require('dotenv');
-const dotenv =require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 // allow cross domain access
 var allowCrossDomain = function (req, res, next) {
@@ -30,19 +30,20 @@ const esUtils = require('./utility/ES-utils.js');
 app.use('/graph/', custom.router);
 app.use('/entity/', client_routes);
 
-app.get('/external/:external_id/:type', async (req,res)=>{
+app.get('/external/:external_id/:type', async (req, res) => {
   let finalData = [];
-  const cypher_query = 'MATCH (n:owl__Class)-[:rdfs__subClassOf]->(p:owl__Class)' +
-    "where  n.ns3__hasDbXref =  '" + req.params.external_id + "'" +
-    ' return n.skos__notation as curie, n.ns3__hasDbXref as search_id'
+  const exter_id = "'" + req.params.external_id + "'"
+  const atlas = exter_id.split(":")[0]
+  const id = exter_id.split(":")[1]
+  const cypher_query = ' MATCH (atlas)<-[definedby:subClassOf]-(label)-[:`http://uri.interlex.org/tgbugs/uris/readable/delineates`]->(anatomy)' +
+    ' WHERE toUpper(atlas.abbreviation) = ' + atlas + '\' AND split(label.iri,"/")[-1] = \'' + id + ' AND exists(atlas.abbreviation)' +
+    ' RETURN anatomy.`http://www.geneontology.org/formats/oboInOwl#id` as curie, toUpper(atlas.abbreviation) + ":" + split(label.iri,"/")[-1] as search_id'
   const data = await custom.runGraphQuery(cypher_query, "NIFSTD");
-  console.debug("data for curie");
-  console.debug(data);
   if (data[0] && data[0]._fields) {
     const curie = data[0]._fields[0]; // for curie
     const slug = await esUtils.findSlugByCurie(curie);
     const slugDetails = await esUtils.findBySlug(slug);
-    finalData = await esUtils.getSpecificDetails(slugDetails, req.params.type,slug);
+    finalData = await esUtils.getSpecificDetails(slugDetails, req.params.type, slug);
   }
   res.send(finalData);
 })
