50de316eca8eb911c4068ef9c9121c4e507505c4
Visakh Muraleedharan <visakh@incf.org>
2022-04-27 14:46:10 +0200

    updated index

--- server/routes/client-routes.js
+++ server/routes/client-routes.js
@@ -135,7 +135,7 @@ router.get("/source-data-by-entity", function (req, res) {
 router.get("/literature-by-curie-paths", function (req, res) {
   esDataSpaceClient
     .search({
-      index: "test_pubmed",
+      index: "pubmed_22",
       type: "_doc", // 'publication',
       body: req.query.body,
     })
