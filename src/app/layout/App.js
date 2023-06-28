8e3434d15ea99eb000afc4464f43ebcc122b40de
sumit neema <sumit@incf.org>
2023-06-28 12:27:18 +0200

    add cookie consent

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,29 +1,62 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+import CookieConsent, {
+  getCookieConsentValue,
+  Cookies,
+} from "react-cookie-consent";
 
-import withRoot from 'withRoot'
-import Typography from '@material-ui/core/Typography'
-import {withStyles} from '@material-ui/core/styles'
+import withRoot from "withRoot";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
-import Main from './Main'
-import GA from 'features/googleAnalytics/GoogleAnalytics'
+import Main from "./Main";
+import GA from "features/googleAnalytics/GoogleAnalytics";
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    textAlign: 'center'
-  }
-})
+    textAlign: "center",
+  },
+});
 
 class App extends Component {
+  state = {
+    isConsent: false,
+  };
+
+  componentDidMount() {
+    const isConsent = getCookieConsentValue();
+    if (isConsent === "true") {
+      this.setState({ isConsent: true });
+    }
+  }
+
+  handleAcceptCookie = () => {
+    this.setState({ isConsent: true });
+  };
+
+  handleDeclineCookie = () => {
+    //remove google analytics cookies
+    Cookies.remove("_ga");
+    Cookies.remove("_gat");
+    Cookies.remove("_gid");
+  };
+
   render() {
-	  const {classes} = this.props
+    const { classes } = this.props;
 
     return (
       <div className={classes.root}>
-        { GA.init() && <GA.RouteTracker /> }
-        <Main/>
+        {this.state.isConsent && GA.init() && <GA.RouteTracker />}
+        <Main />
+        <CookieConsent
+          enableDeclineButton
+          onAccept={this.handleAcceptCookie.bind(this)}
+          onDecline={this.handleDeclineCookie.bind(this)}
+        >
+          This website uses cookies to enhance the user experience.
+        </CookieConsent>
       </div>
-    )
+    );
   }
 }
 
-export default withRoot(withStyles(styles)(App))
+export default withRoot(withStyles(styles)(App));
